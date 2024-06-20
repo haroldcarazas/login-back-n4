7ae2571672e2d319a341ca0cb104d2c37bbc4ec9
harold2001 <hcarazasnires@gmail.com>
2024-06-20 13:00:07 -0500

    login y jwt

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,4 +1,6 @@
+import { SECRET_KEY } from '../config/config.js'
 import { pool } from '../config/db.js'
+import jwt from 'jsonwebtoken'
 
 export const register = async (req, res) => {
   try {
@@ -17,6 +19,24 @@ export const register = async (req, res) => {
     res.json({ message: 'Usuario registrado con éxito' })
   } catch (error) {
     if (error?.errno === 1062) return res.status(400).json({ message: 'El nombre de usuario ya existe' })
+    res.status(500).json({ message: error.message })
+  }
+}
+
+export const login = async (req, res) => {
+  try {
+    const { username, password } = req.body
+
+    const [resultado] = await pool.execute('SELECT * FROM usuarios WHERE username=?', [username])
+    if (resultado.length === 0) return res.status(400).json({ message: 'Usuario no encontrado' })
+
+    const usuario = resultado[0]
+    if (password !== usuario.password) return res.status(400).json({ message: 'Credenciales inválidas' })
+
+    const token = jwt.sign({ usuarioId: usuario.usuario_id }, SECRET_KEY, { expiresIn: '60' })
+
+    res.json({ message: 'Usuario autenticado', token })
+  } catch (error) {
     res.status(500).json({ message: error.message })
   }
 }
